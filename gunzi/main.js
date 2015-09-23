2c225db8f890b8f229b618e85f6a76b9c9af40e4
yjh <longwenjunj@163.com>
2015-09-23 16:47:58 +0800

    add gunzi

---
+++ gunzi/main.js
@@ -0,0 +1,14 @@
+cc.game.onStart = function(){
+    if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
+        document.body.removeChild(document.getElementById("cocosLoading"));
+
+    var designSize = cc.size(480, 800);
+    cc.loader.resPath = "res";
+    cc.view.setDesignResolutionSize(designSize.width, designSize.height, cc.ResolutionPolicy.SHOW_ALL);
+
+    //load resources
+    cc.LoaderScene.preload(g_resources, function () {
+        cc.director.runScene(new StartScene());
+    }, this);
+};
+cc.game.run();
